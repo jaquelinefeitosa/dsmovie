a22b76200cb580079542cafa43441f8037666329
Jaqueline <jaqueline.feitosa.sousa@gmail.com>
2022-01-15 23:52:36 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,26 @@
 import axios from "axios";
 import MovieCard from "components/Navbar/MovieCard";
 import Pagination from "components/Navbar/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get("https://jack-dsmovie.herokuapp.com")
-    .then(response => {
-        console.log(response.data);
-    });
+    const[pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number); 
+        });
+    }, []);   
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
